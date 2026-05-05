9b1345b5f75e0cf4696dc991c8bea21026778c5f
Evan ngugi wambui <evanngugi547@gmail.com>
2026-05-05 11:10:49 +0300

    switch to vite-pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,37 @@
+import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    strategies: 'injectManifest',
+    srcDir: 'src',
+    filename: 'sw.ts',
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'bookstore',
+      short_name: 'duka',
+      description: 'Modern POS for a book and stationery shop in Kenya',
+      theme_color: '#ffffff',
+    },
+
+    injectManifest: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
 })
