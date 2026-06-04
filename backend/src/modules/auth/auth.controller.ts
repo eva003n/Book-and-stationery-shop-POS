17e3c079644364b049eb340607799d6ad2be4fe3
Evan ngugi wambui <evanngugi547@gmail.com>
2026-06-04 21:15:12 +0300

    add auth controller

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import asyncHandler from "../../shared/utils/asynchandler.js";
 import ApiError from "../../shared/utils/ApiError.js";
 import {
@@ -9,20 +9,22 @@ import {
 } from "../../shared/validator/validators.js";
 import { logInUser, registerUser, signOutUser } from "./auth.service.js";
 import ApiResponse from "../../shared/utils/ApiResponse.js";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./auth.config.js";
 
 export const signUp = asyncHandler(async (req: Request, res: Response) => {
-
   const data = await registerUser(req.validated?.body as SignUpAuth);
 
-  return res.status(201).json(new ApiResponse({
-    message: "User registered successfully",
-    data,
-    meta: { requestId: req.requestId },
-  }));
+  return res.status(201).json(
+    new ApiResponse({
+      message: "User registered successfully",
+      data,
+      meta: { requestId: req.requestId },
+    }),
+  );
 });
 
 export const signIn = asyncHandler(async (req: Request, res: Response) => {
-
   const data = await logInUser(req.validated?.body as SignInAuth, req.headers);
 
   return res.status(200).json(
@@ -37,9 +39,29 @@ export const signIn = asyncHandler(async (req: Request, res: Response) => {
 export const signOut = asyncHandler(async (req: Request, res: Response) => {
   await signOutUser(req.headers);
 
-  return res.status(200).json(new ApiResponse({
-    message: "User signed out successfully",
-    data: null,
-    meta: { requestId: req.requestId },
-  }));
+  return res.status(200).json(
+    new ApiResponse({
+      message: "User signed out successfully",
+      data: null,
+      meta: { requestId: req.requestId },
+    }),
+  );
 });
+
+// const handler = toNodeHandler(auth);
+
+export const authHandler = toNodeHandler(auth);
+/* asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const originalJson = res.json.bind(res);
+    res.json = (body) => {
+      if (res.statusCode >= 400) {
+        return originalJson(ApiError.badRequest(req.originalUrl, undefined, body))
+      }
+
+      return originalJson(new ApiResponse(body));
+    };
+
+    return await handler(req, res);
+  },
+); */
