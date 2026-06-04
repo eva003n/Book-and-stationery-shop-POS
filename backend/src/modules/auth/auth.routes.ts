8fa50a56809dd7652c8e9b857ea85e67558307d6
Evan ngugi wambui <evanngugi547@gmail.com>
2026-06-04 21:13:49 +0300

    moute validate middleware per endpoint

--- backend/src/modules/auth/auth.routes.ts
+++ backend/src/modules/auth/auth.routes.ts
@@ -2,9 +2,17 @@
 // import { signIn, signOut, signUp } from "./auth.controller.js";
 // import { validate } from "../../middleware/validate.middleware.js";
 // import { signInSchema, signUpSchema, type SignInAuth, type SignUpAuth } from "../../shared/validator/validators.js";
-import { auth } from "./auth.config.js";
+import { Router } from "express";
 
-const router = auth.handler
+import { authHandler } from "./auth.controller.js";
+import { validate } from "../../middleware/validate.middleware.js";
+import { signInSchema, signUpSchema } from "../../shared/validator/validators.js";
+
+const router = Router()
+
+router.route("/sign-up/email").post(validate(signUpSchema))
+router.route("/sign-in/email").post(validate(signInSchema))
+router.use("/", authHandler);
 
 // router.route("/sign-up").post(validate({body: signUpSchema}), signUp)
 // router.route("/sign-in").post(validate({body: signInSchema}), signIn)
